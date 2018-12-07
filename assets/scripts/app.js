c0a2830ae5087c233edb45e5debee34ce199e07b
liquidsteel49 <lowercasepeople49@gmail.com>
2018-12-07 16:44:38 -0500

    added event listener for sign up sign up is correct

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,68 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const main = require('./main.js')
+
 $(() => {
-  // your JS code goes here
+  function httpFetch(url, body, verb) {
+    let myHeaders = new Headers()
+    myHeaders.set('Content-Type', 'application/json')
+    let myInit = { cache: 'default', headers: myHeaders, method: verb, mode: 'cors' }
+    if (body) {
+      myInit.body = JSON.stringify(body)
+    }
+    const myRequest = new Request(url, myInit)
+    return fetch(myRequest)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response.json()
+      })
+  }
+
+  const url = "http://localhost:4741/sign-up"
+  const data = {
+    "credentials": {
+      "email": "test4@test.com",
+      "password": "test",
+      "password_confirmation": "test"
+    }
+  }
+
+
+
+// httpFetch(url, data, "POST")
+//   .then((result) => {
+//     console.log(result)
+//   })
+//   .catch((result) => {
+//     console.error(result)
+//   })
+
+
+/*
+  $.ajax({
+    url: "http://localhost:4741/sign-up",
+    data: {
+      "credentials": {
+        "email": "test5@test.com",
+        "password": "test",
+        "password_confirmation": "test"
+      }
+    },
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    success: (result) => {
+      console.log(result)
+    },
+    error: (result) => {
+      console.error(result)
+    }
+});
+*/
+
+$('#user-registry').on('submit', main.signUp)
 })
